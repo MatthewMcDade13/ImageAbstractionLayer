f1562bf727f39b624a3459f57fc5abad2469cdf6
Matthew McDade <Matthew.McDade13@gmail.com>
2017-04-27 03:47:40 -0700

    Added Repository for Context Class

---
+++ ImgAbstractionLayer/Models/ISearchRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgAbstractionLayer.Models
+{
+    public interface ISearchRepository
+    {
+        IEnumerable<Search> GetAllSearches();
+
+        void AddSearch(Search search);
+
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ ImgAbstractionLayer/Models/SearchRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgAbstractionLayer.Models
+{
+    public class SearchRepository : ISearchRepository
+    {
+        SearchContext context;
+
+        public SearchRepository(SearchContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Search> GetAllSearches()
+        {
+            return context.Searches.ToList();
+        }
+
+        public void AddSearch(Search search)
+        {
+            context.Add(search);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await context.SaveChangesAsync()) > 0;
+        }
+    }
+}
