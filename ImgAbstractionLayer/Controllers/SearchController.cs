fbf89847ccf87db12df542e924ea493ec431d44b
Matthew McDade <Matthew.McDade13@gmail.com>
2017-04-27 21:02:25 -0700

    Added an api method to call an external API and return its results in json format

--- ImgAbstractionLayer/Controllers/SearchController.cs
+++ ImgAbstractionLayer/Controllers/SearchController.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace ImgAbstractionLayer.Controllers
 {
@@ -18,8 +21,26 @@ namespace ImgAbstractionLayer.Controllers
             this.repo = repo;
         }
 
-        //GET api/imagesearch/latest
-        [HttpGet("latest")]
+        [HttpGet("{term:alpha}")]
+        public async Task<IActionResult> SearchImages(string term)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync("https://wind-bow.glitch.me/twitch-api/channels/classypax"))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    string jsonString = await response.Content.ReadAsStringAsync();
+
+                    Object json = JsonConvert.DeserializeObject(jsonString);
+
+                    return Json(json);
+                }
+            }
+        }
+
+        //GET api/imagesearch/recent
+        [HttpGet("recent")]
         public IActionResult GetRecentSearches()
         {
 
